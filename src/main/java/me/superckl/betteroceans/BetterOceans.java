f754010b9694af79df65283fdab1b4e0e0195398
Christopher Larsson <clarsson@wisc.edu>
2014-07-08 22:53:54 -0500

    More setup

--- src/main/java/me/superckl/betteroceans/BetterOceans.java
+++ src/main/java/me/superckl/betteroceans/BetterOceans.java
@@ -1,8 +1,36 @@
 package me.superckl.betteroceans;
 
+import lombok.Getter;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid="BetterOceans", name="Better Oceans", version="1.0")
 public class BetterOceans {
-
+	
+	@Instance("BetterOceans")
+	@Getter
+	private static BetterOceans instance;
+	
+	@Getter
+	private Config config;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e){
+		this.config = new Config();
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent e){
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent e){
+		
+	}
+	
 }
---
+++ src/main/java/me/superckl/betteroceans/Config.java
@@ -0,0 +1,5 @@
+package me.superckl.betteroceans;
+
+public class Config {
+
+}
