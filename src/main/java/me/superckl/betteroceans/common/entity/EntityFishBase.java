b513b15c35e9c1cf575b9dc20c640148cb862ae3
superckl <lars4647@umn.edu>
2014-08-22 02:49:43 -0700

    Starting fish

---
+++ src/main/java/me/superckl/betteroceans/common/entity/EntityFishBase.java
@@ -0,0 +1,44 @@
+package me.superckl.betteroceans.common.entity;
+
+import lombok.Getter;
+import me.superckl.betteroceans.common.entity.ai.EntityAIHeightRestrictedWander;
+import me.superckl.betteroceans.common.reference.ModItems;
+import me.superckl.betteroceans.common.utility.BlockHelper;
+import net.minecraft.block.Block;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.EntityAIAvoidEntity;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAITempt;
+import net.minecraft.entity.passive.EntityWaterMob;
+import net.minecraft.entity.passive.IAnimals;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+
+public class EntityFishBase extends EntityWaterMob{
+
+	@Getter
+	private final boolean canLiveInSaltWater;
+	
+	public EntityFishBase(World world, boolean canLiveInSaltWater) {
+		super(world);
+		this.canLiveInSaltWater = canLiveInSaltWater;
+		//this.tasks.addTask(2, new EntityAIHeightRestrictedWander(this, 0.8D, 25));
+		this.tasks.addTask(1, new EntityAISwimming(this));
+		//this.tasks.addTask(3, new EntityAITempt(this, 0.85D, ModItems.itemSeaweed, false));
+		//this.tasks.addTask(4, new EntityAIAvoidEntity(this, EntityPlayer.class, 5, 0.81D, 1.2D)); Will be used in subclasses
+	}
+
+	@Override
+	public boolean getCanSpawnHere() {
+		Block block = this.worldObj.getBlock((int) this.posX, (int) this.posY, (int) this.posZ);
+		return this.canLiveInSaltWater ? BlockHelper.isWaterSource(block):block == Blocks.water;
+	}
+
+	@Override
+	public boolean canBeSteered() {
+		return false;
+	}
+	
+}
---
+++ src/main/java/me/superckl/betteroceans/common/entity/ai/EntityAIGroupWith.java
@@ -0,0 +1,13 @@
+package me.superckl.betteroceans.common.entity.ai;
+
+import net.minecraft.entity.ai.EntityAIBase;
+
+public class EntityAIGroupWith extends EntityAIBase{
+
+	@Override
+	public boolean shouldExecute() {
+		// TODO 
+		return false;
+	}
+
+}
